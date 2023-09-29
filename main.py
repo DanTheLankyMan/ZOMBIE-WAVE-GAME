9916e5214020213a4fb99b92c79334f182d88d64
DanTheLankyMan <139452864+DanTheLankyMan@users.noreply.github.com>
2023-09-29 11:23:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import pygame
+import pygame as pg
 import sys
 from config import *
 from sprites import *
 
 class Game:
     def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption(TITLE)
-        self.clock = pygame.time.Clock()
-        pygame.key.set_repeat(500, 100)
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
         self.load_data()
 
     def load_data(self):
@@ -17,14 +17,12 @@ class Game:
 
     def new(self):
         # initialize all variables and do all the setup for a new game
-        self.all_sprites = pygame.sprite.Group()
-        self.walls = pygame.sprite.Group()
-        self.player = Player(self, 10, 10)
-        for x in range(10, 20):
-            Wall(self, x, 5)
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player = Player(self, 620, 330)
 
     def run(self):
-        # game loop - set self.playing = False to end the game
+        # game loop
         self.playing = True
         while self.playing:
             self.dt = self.clock.tick(FPS) / 1000
@@ -34,42 +32,30 @@ class Game:
 
     def quit(self):
         # used to close window and stop code from running
-        pygame.quit()
+        pg.quit()
         sys.exit()
 
     def update(self):
         # update portion of the game loop
         self.all_sprites.update()
 
-    def draw_grid(self):
-        # draws tile grid
-        for x in range(0, WIDTH, TILESIZE):
-            pygame.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pygame.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
 
     def draw(self):
+        # draws everything
         self.screen.fill(BACKROUNDCOLOUR)
-        self.draw_grid()
         self.all_sprites.draw(self.screen)
-        pygame.display.flip()
+        pg.display.flip()
 
     def events(self):
-        # catch all events here
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        #  all events ended here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 self.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
                     self.quit()
-                if event.key == pygame.K_LEFT:
-                    self.player.move(dx=-1)
-                if event.key == pygame.K_RIGHT:
-                    self.player.move(dx=1)
-                if event.key == pygame.K_UP:
-                    self.player.move(dy=-1)
-                if event.key == pygame.K_DOWN:
-                    self.player.move(dy=1)
+
 
     def show_start_screen(self):
         pass
--- sprites.py
+++ sprites.py
@@ -1,31 +1,52 @@
-import pygame
+import pygame as pg
 from config import *
 
-class Player(pygame.sprite.Sprite):
+class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
+        # initiate sprites
         self.groups = game.all_sprites
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
+        self.vx = 0
+        self.vy = 0
+
+
+    def used_keys(self):
+        # sets keys for the movement and the speed at which they will cause the player to move
+        self.vx = 0
+        self.vy = 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vx= -PLAYER_SPEED
+        elif keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx= PLAYER_SPEED
+        elif keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy= -PLAYER_SPEED
+        elif keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy= PLAYER_SPEED
 
     def move(self, dx=0, dy=0):
         self.x += dx
         self.y += dy
 
     def update(self):
-        self.rect.x = self.x * TILESIZE
-        self.rect.y = self.y * TILESIZE
+        # updates sprites when moving (also matches them to game speed)
+        self.used_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.bottomright= (self.x,self.y)
 
-class Wall(pygame.sprite.Sprite):
+class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.walls
-        pygame.sprite.Sprite.__init__(self, self.groups)
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.x = x
